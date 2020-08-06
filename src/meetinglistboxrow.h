7ec00bb00eaaed29a4faa3ab1a7ebd4709512ced
Ignacio Casal Quinteiro <qignacio@amazon.com>
2020-08-06 16:49:10 +0200

    missing copyrights

--- src/meetinglistboxrow.h
+++ src/meetinglistboxrow.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2020 Amazon.com, Inc. or its affiliates.
+ *
+ * Authors: Ignacio Casal Quinteiro <icq@gnome.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 #ifndef CHIME_MEETING_LIST_BOX_ROW_H
 #define CHIME_MEETING_LIST_BOX_ROW_H
 
